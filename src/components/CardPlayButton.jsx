6e624976d486a855158229ccc6aa10fe440871e9
Daniel Muniz <danielnubiz902@gmail.com>
2023-12-25 23:10:50 -0500

    Add code to spotify clone

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,26 @@
+import { Pause, Play } from "./Player";
+import { usePlayerStore } from "../store/playerStore";
+export function CardPlayButton({ id }) {
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore((state) => state);
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false);
+      return;
+    }
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { songs, playlist } = data;
+        setIsPlaying(true);
+        setCurrentMusic({ songs, playlist, song: songs[0] });
+      });
+  };
+
+  return (
+    <button className="card-play-button rounded-full bg-green-500 p-4 " onClick={handleClick}>
+      {isPlayingPlaylist ? <Pause /> : <Play />}
+    </button>
+  );
+}
